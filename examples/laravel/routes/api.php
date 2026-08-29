5a2394ceafeb3bf1aaf2f1c13a9e24c0736eb6b1
unknown <iandunca99@gmail.com>
2026-08-29 17:41:17 +0300

    Initial commit

--- examples/laravel/routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\PesapalPaymentController;
-use Illuminate\Support\Facades\Route;
-
-Route::match(['GET', 'POST'], '/payments/pesapal/ipn', [PesapalPaymentController::class, 'ipn'])
-    ->name('pesapal.ipn');
--- examples/laravel/routes/console.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Artisan;
-
-Artisan::command('about-example', function (): void {
-    $this->info('Pesapal Laravel SDK runnable example.');
-})->purpose('Describe this example application');
